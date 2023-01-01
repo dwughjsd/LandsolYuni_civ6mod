ebfbb3e41d54872ee744d3bc16534e95a9e390cb
dwughjsd <94778865+dwughjsd@users.noreply.github.com>
2023-01-01 13:37:19 +0800

    Revert

--- LandsolPrinFesPack/LandsolPrinFesPack/UI/Addition/PrinFes_KyoukaSummer_UI.lua
+++
@@ -1,5 +0,0 @@
-ExposedMembers.AquaFlock = {
-	GetBuildingsOnPlot = function (iPlayerID :number, iCityID :number, iPlotID :number)
-		return CityManager.GetCity(iPlayerID,iCityID):GetBuildings():GetBuildingsAtLocation(iPlotID);
-	end
-};
